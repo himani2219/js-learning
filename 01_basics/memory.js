2a20f911d281fcd997b7bbf8c00ff93ac14197df
Himani <102609895+himani2219@users.noreply.github.com>
2024-03-21 19:17:17 +0000

    stack and heap memory

---
+++ 01_basics/memory.js
@@ -0,0 +1,9 @@
+let userone={
+    email : "user@google.com",
+    upi : "user@ybl"
+}
+
+let usertwo = userone;
+usertwo.email="himani@gmail.com";
+console.log(userone.email);
+console.log(usertwo.email);
