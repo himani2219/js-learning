7df88ca15d82fda2386ff8369f714fa008b17225
Himani <102609895+himani2219@users.noreply.github.com>
2024-03-26 20:29:31 +0000

    .

---
+++ function/function.js
@@ -0,0 +1,20 @@
+function a(){
+    console.log("a")
+    console.log("b")
+    console.log("c")
+    console.log("d")
+    console.log("e")
+}
+//a()
+
+function add(n1, n2){
+    //parameters
+    console.log(n1+n2)
+}
+// add(1,34)
+//arguments
+
+function login(username){
+    return `${username} just logged in.`
+}
+console.log(login("Neha"))
